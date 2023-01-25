763e190e26f4a61b332e428f65413a8b220722b5
map19042 <map19042@map19042-vm.cse.uconn.edu>
2023-01-25 12:31:45 -0500

    first commit

---
+++ prefix_sum.h
@@ -0,0 +1,9 @@
+#ifndef __PREFIX_SUM_H
+#define __PREFIX_SUM_H
+
+
+int prefix_sum(int array[], int length); // last must be less than the length of the array
+bool non_negative_sum(int array[], int length);
+bool non_positive_sum(int array[], int length);
+
+#endif
